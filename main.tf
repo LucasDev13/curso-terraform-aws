09dca992c04c6fb0ef72701cd68b0f70d5160227
Lucas <lucaspontes140@gmail.com>
2022-03-18 10:19:14 -0300

    Alterado o nome do bucket.

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-12345678910885522"
+  bucket = "my-tf-test-bucket-12345678910885522123123123123"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-    Managedby = "Terraform"
+    ManagedBy   = "Terraform"
+    Owner       = "Lucas Pontes"
+    UpdatedAt   = "2022-03-18"
   }
 }
